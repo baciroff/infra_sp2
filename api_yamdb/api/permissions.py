334d0d920807c803b63fa395c1609b438ef7bfe9
Руслан Бакиров <baciroff82@gmail.com>
2023-05-16 00:25:10 +0300

    Работа с контейнерами

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -21,19 +21,6 @@ class IsAdminOrModeratorOrReadOnly(permissions.BasePermission):
                 or obj.author == request.user)
 
 
-class IsAdminOrModeratorOrReadOnly(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        return (request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated)
-
-    def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS
-                or request.user.is_moderator
-                or request.user.is_admin
-                or obj.author == request.user)
-
-
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and (
