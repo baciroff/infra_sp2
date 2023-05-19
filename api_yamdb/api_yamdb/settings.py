fb6e0dd50d903dec7ae4d86d63d5e72798bd56c1
Руслан Бакиров <baciroff82@gmail.com>
2023-05-19 08:34:55 +0300

    Работа над замеаниями №2

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -113,10 +113,10 @@ STATIC_URL = '/static/'
 
 # STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
